4ef5c46e47bc3e86029f76c8c0c90d8253e78256
mi-uhu <michael.unterhuber94@gmail.com>
2021-11-26 14:14:35 +0100

    added @BeforeEach AccountTest.initialize to AccountTest.java added @Test AccountTest.testConstructor to AccountTest.java

--- src/test/java/org/campus02/unterhuber/AccountTest.java
+++ src/test/java/org/campus02/unterhuber/AccountTest.java
@@ -1,9 +1,23 @@
 package org.campus02.unterhuber;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountTest {
+    private Account accountMichael;
+
+    @BeforeEach
+    void initialize() {
+        this.accountMichael = new Account("Michael");
+    }
+
+    @Test
+    void testConstructor() {
+        Assertions.assertEquals("Michael", accountMichael.getOwner());
+        Assertions.assertEquals(100.0, accountMichael.getAmount());
+    }
 
 }
